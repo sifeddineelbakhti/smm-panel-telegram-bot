421715b02e87db4eca579fe13bb465d8aa93dab2
sifeddineelbakhti <157433673+sifeddineelbakhti@users.noreply.github.com>
2024-01-23 01:30:16 +0100

    Update _UnbanUser.js

--- commands/Adminpanel/_UnbanUser.js
+++ commands/Adminpanel/_UnbanUser.js
@@ -13,7 +13,7 @@
 CMD*/
 
 var badUsers = Bot.getProperty("mods", { list: {} })
-if (!badUsers.list[user.telegramid] && user.telegramid !=2016839211) {
+if (!badUsers.list[user.telegramid] && user.telegramid !=6547961721) {
   Api.sendPhoto({
     photo: "https://images.app.goo.gl/4rcN9nn7G1skV28J9",
     caption: "<b>You were demoted</b>",
@@ -23,7 +23,7 @@ if (!badUsers.list[user.telegramid] && user.telegramid !=2016839211) {
   return
 } else
 var badUsers = Bot.getProperty("blacklisted", { list: {} })
-if (badUsers.list[message] && user.telegramid != 2016839211) {
+if (badUsers.list[message] && user.telegramid != 6547961721) {
   Api.sendPhoto({
     photo: "https://imbt.ga/0FrY3m2i75",
     caption: "<b>User cannot be unblocked!</b>",
@@ -38,7 +38,7 @@ Bot.sendMessage(
 )
 Bot.sendMessageToChatWithId(tgID,"*☺️ You Have Been Unbanned Now In This Bot*")
 Api.sendMessage({
-  chat_id: "2016839211",
+  chat_id: "6547961721",
   text: "*@" +
     user.telegramid +
     " Unbanned: " + message + "*",
